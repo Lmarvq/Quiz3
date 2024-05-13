4a606cd8de590117a8edff92f646fca02d8fa5a4
Lmarvq <luisa.martinezv1@udea.edu.co>
2024-05-13 17:58:37 -0500

    Correciones aplicadas para la conversión de dicom a nifti

--- clases.py
+++ clases.py
@@ -26,7 +26,7 @@ class Paciente():
     def AddID(self, i):
         self.__ID = i
     def Add_IM_Dicom(self, d):
-        self.__IM_DICOM = d
+        self.__IM_DICOM = [d]
     def Add_IM_Niftii(self, f):
         self.__IM_Niftii = f
 
@@ -45,14 +45,8 @@ p = Paciente()
     
 def Leer_Asignar_Info():
     dirD = input(r"Ingrese la dirección del directorio Dicom: ")
-    op1 = int(input("¿Desea crear un elemento Niftii a partir del archivo anterior?:\n 1.Si\n 2.No\n Usted ingresó: "))
-    if op1 ==1:
-        dirN = input(r"Ingrese la dirección donde desea almacenar elementos Niftii: ")
-        f = d2n.convert_directory(dirD, dirN)
-        p.Add_IM_Niftii(f)
-    else: 
-        pass
-    ds = pyd.dcmread(dirD)
+    nom = input("Ingrese el nombre de uno de los archivos dicom para extraer la información: ")
+    ds = pyd.dcmread(os.path.join(dirD,nom))
     n = ds.PatientName
     e = ds.PatientAge
     i = ds.PatientID
@@ -65,15 +59,15 @@ def Leer_Asignar_Info():
     
     dict__pacientes[p.ObtenerID] = p
     dict__imágenes[p.ObtenerID] = [p.Obtener_IM_Dicom]
+    op1 = int(input("¿Desea crear un elemento Niftii a partir del archivo anterior?:\n 1.Si\n 2.No\n Usted ingresó: "))
+    if op1 ==1:
+        dirN = input(r"Ingrese la dirección donde desea almacenar elementos Niftii: ")
+        f = d2n.convert_directory(dirD,dirN)
+        p.Add_IM_Niftii(f)
+    else: 
+        pass
     print("Los datos han sido guardados correctamente")
-    op2 = int(input("¿Desea visualizar la imágen asociada al paciente?\n 1.Si\n 2.No\n Usted escogió: "))
-    if op2 == 1:
-        Im = np.array(d)
-        plt.imshow(Im)
-        plt.show()
-        pass
-    if op2 ==2 : 
-        pass
+
 
 def Asignar_Im():
     dir = input(r"Ingrese la dirección de la imágen JPG o PNG que desea registrar: ")
